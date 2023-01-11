c84c090c9f6957cd13920ee789e6a049a67615b5
jlassig <las22004@byui.edu>
2023-01-10 19:19:05 -0700

    Resume program from learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2010;
+        job1._endYear = 2022;
+        // job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._company = "USPS";
+        job2._jobTitle = "Delivery Consultant";
+        job2._startYear = 1980;
+        job2._endYear = 2010;
+        // job2.DisplayJobDetails();
+
+        Resume myResume = new Resume();
+        myResume._name = "Bob Smith";
+        myResume._jobList.Add(job1);
+        myResume._jobList.Add(job2);
+        myResume.DisplayResume();
+
     }
 }
