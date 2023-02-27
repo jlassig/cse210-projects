7c575b05643c07145422f7b7cd93c1b1e4c0b092
jlassig <las22004@byui.edu>
2023-02-27 07:35:27 -0700

    shape program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using static System.Console;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapeList = new List<Shape>();
+
+
+        Square square = new Square("blue", 10);
+        Rectangle rectangle = new Rectangle("orange", 10,5);
+        Circle circle = new Circle("yellow", 20);
+        shapeList.Add(square);
+        shapeList.Add(rectangle);        
+        shapeList.Add(circle);
+
+        foreach(Shape shape in shapeList)
+        {
+            WriteLine($"Color: {shape.GetColor()} -- Area: {shape.GetArea()}");
+        }
+
+
+       
+
+
+
+
+
+
     }
 }
