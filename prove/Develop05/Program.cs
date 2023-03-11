514120230a1446713fe00ba2ce7ae40a709f73fa
jlassig <las22004@byui.edu>
2023-03-10 20:33:42 -0700

    added a comment

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,5 +1,6 @@
 //I added a fun animation for the user whenever they record an event because I think that every. single. time. is worth celebrating! 
 //I also added a way for the user to delete a goal, especially the ones they've already achieved like the checklist goal and the eternal goal. 
+//I added a method called GetFileName, so the user only had to enter the fileName once per session. 
 
 using System;
 
